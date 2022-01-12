505cb6e78f6565e2d4c9519e13ac3c1710acd2ee
Sam <sam.a.fletcher@gmail.com>
2022-01-12 20:17:18 +0000

    Test

--- bruce/src/components/HelloWorld.vue
+++ bruce/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>Hello World</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
@@ -112,7 +112,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String,
   },
